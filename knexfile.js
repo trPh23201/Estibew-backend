d3f543c99d9183c739cdc5badb0a3d7cbf81fe39
tripham <gnoob53@example.com>
2023-04-12 14:50:56 +0700

    settup for remote

--- knexfile.js
+++ knexfile.js
@@ -8,9 +8,12 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
+      ssl: { rejectUnauthorized: false},
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT,
       database: process.env.DATABASE_NAME,
-      user: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD
+      password: process.env.DATABASE_PASSWORD,
+      user: process.env.DATABASE_USER
     },
     pool: {
       min: 2,
@@ -19,5 +22,12 @@ module.exports = {
     migrations: {
       tableName: 'knex_migrations'
     }
-  }
+  },
+  ...(1 ? console.log({
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    user: process.env.DATABASE_USER
+  }): undefined)
 };
