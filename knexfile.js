c8fe17ba329ce8ee6e606281296a43a6001bae98
tripham <gnoob53@example.com>
2023-04-12 15:23:08 +0700

    fix config

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,32 @@ require('dotenv').config()
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
+  ...(1 ? console.log({
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    user: process.env.DATABASE_USER,
+    ...(process.env.NODE_ENV != 'development' ? {ssl: { rejectUnauthorized: false}} : undefined),
+  }): undefined),
   development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT,
+      database: process.env.DATABASE_NAME,
+      password: process.env.DATABASE_PASSWORD,
+      user: process.env.DATABASE_USER
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+  stagging: {
     client: 'pg',
     connection: {
       ssl: { rejectUnauthorized: false},
@@ -22,12 +47,5 @@ module.exports = {
     migrations: {
       tableName: 'knex_migrations'
     }
-  },
-  ...(1 ? console.log({
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    database: process.env.DATABASE_NAME,
-    password: process.env.DATABASE_PASSWORD,
-    user: process.env.DATABASE_USER
-  }): undefined)
+  }
 };
