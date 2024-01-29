e65f999a99b8d3905fb443e740afa2355ab20956
tripham <gnoob53@gmail.com>
2024-01-29 11:59:56 +0700

    fix docker migrates & seed

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # How to run with docker
 
-1) docker build -t estibew_backend .
+1. docker build -t estibew_backend .
 
-2) docker-compose up
+2. docker-compose up
+
+3. To gain seed:
+   - Open other terminal
+   - docker exec -d estibew_backend npx knex seed:run
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const http = require('http')
-const server = http.createServer(app)
-const cors = require('cors')
-const morgan = require('morgan')
-const routes = require('./routes')
-const config = require('./setting/config')
-const Knex = require('knex')
-const { Model } = require('objection')
-const middleware = require('./middleware')
-const path = require('path')
-const SocketIOServer = require('./services/socketio')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const cors = require("cors");
+const morgan = require("morgan");
+const routes = require("./routes");
+const config = require("./setting/config");
+const Knex = require("knex");
+const { Model } = require("objection");
+const middleware = require("./middleware");
+const path = require("path");
+const SocketIOServer = require("./services/socketio");
 
 // const session = require('express-session');
 // const passport = require('passport')
@@ -26,34 +26,53 @@ const SocketIOServer = require('./services/socketio')
 // app.use(passport.session())
 
 //Connect database
-const knex = Knex(config.databaseConfig)
-knex.raw('select 1+1 as result').then((data) => {
-    console.log('📁 Database connected!')
-})
-Model.knex(knex)
+const knex = Knex(config.databaseConfig);
+const connect = () => {
+  knex
+    .raw("select 1+1 as result")
+    .then((data) => {
+      console.log("📁 Database connected!");
+      knex.migrate
+        .latest()
+        .then(function (res) {
+          console.log("⛵ migration accomplished!");
+        })
+        .catch((e) => console.log(e));
+    })
+    .catch((error) => {
+      console.log("🚫 db connect error retrying connect");
+      setTimeout(() => {
+        connect();
+      }, 4000);
+    });
+};
+connect();
+Model.knex(knex);
 
-app.use(morgan('tiny'))
-app.use(cors())
+app.use(morgan("tiny"));
+app.use(cors());
 //Body-parser middleware
 app.use(
-    express.urlencoded({
-        extended: true,
-    }),
+  express.urlencoded({
+    extended: true,
+  })
 ); //Form submit HTML
 app.use(express.json()); //For fetch, XMLRequest, axios,....
 
-app.use(express.static(path.join(__dirname, '..', 'public')))
+app.use(express.static(path.join(__dirname, "..", "public")));
 
-app.use(middleware.checkin)
+app.use(middleware.checkin);
 
-app.get('/', function (req, res) {
-    res.status(200).json('Welcome')
-})
-app.use(routes)
+app.get("/", function (req, res) {
+  res.status(200).json("Welcome");
+});
+app.use(routes);
 
 server.listen(process.env.SERVER_PORT || 5000, () => {
-    console.log('⚡ Enviroment:', process.env.NODE_ENV)
-    console.log(`🚀 Server ready at http://localhost:${process.env.SERVER_PORT || 5000}`);
+  console.log("⚡ Enviroment:", process.env.NODE_ENV);
+  console.log(
+    `🚀 Server ready at http://localhost:${process.env.SERVER_PORT || 5000}`
+  );
 });
 
-SocketIOServer.start(server)
+SocketIOServer.start(server);
